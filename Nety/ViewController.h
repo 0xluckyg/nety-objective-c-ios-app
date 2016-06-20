c121b22d27155f3a1a620ce1d4b1d4e752d14191
Scott Cho <scottsgcho@gmail.com>
2016-06-20 02:03:35 -0400

    Initial Commit

---
+++ Nety/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nety
+//
+//  Created by Scott Cho on 6/20/16.
+//  Copyright © 2016 Scott Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
